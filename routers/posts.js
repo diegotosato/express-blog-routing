23b0fa7ebec937a822d86bf5b2bc864c5c880777
diegotosato <tosato1998@gmail.com>
2025-11-25 12:03:20 +0100

    Add CRUD operations for posts in the router

--- routers/posts.js
+++ routers/posts.js
@@ -58,8 +58,32 @@ Tutte le risposte saranno dei testi che confermeranno l’operazione che il serv
 
 //index
 router.get('/', (req, res) => {
-  res.send('Mostrami tutti i post')
+  res.send('Lista dei i post')
 })
 
+//show
+router.get('/:id', (req, res) => {
+  res.send(`Mostrami il post con id: ${req.params.id}`)
+})
+
+//store
+router.post('/', (req, res) => {
+  res.send('Aggiungi un nuovo post')
+})
+
+//update
+router.put('/:id', (req, res) => {
+  res.send(`Modifica INTERAMENTE il post con id: ${req.params.id}`)
+})
+
+//modify
+router.patch('/:id', (req, res) => {
+  res.send(`Modifica PARZIALMENTE il post con id: ${req.params.id}`)
+})
+
+//destroy
+router.delete('/:id', (req, res) => {
+  res.send(`Cancellazione del post con id: ${req.params.id}`)
+})
 
 module.exports = router;
